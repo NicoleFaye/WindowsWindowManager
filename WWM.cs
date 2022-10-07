7235e11f2d4ef55f4e9337f329f5af5e0de8aaea
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2022-10-06 23:02:56 -0700

    Remove v1

--- WWM.cs
+++ WWM.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsWindowManagerV1
+namespace WindowsWindowManager
 {
     public class WWM
     {
